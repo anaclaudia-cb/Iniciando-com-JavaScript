96cb61c575f3962b4bbd5124f9ceb72944d8b6d9
Ana Claudia <anaclaudia@yahp.com.br>
2021-12-16 15:32:37 -0300

    ...

--- Curso_EspecializaTi/03/1-operadores.js
+++ Curso_EspecializaTi/03/1-operadores.js
@@ -5,3 +5,4 @@ console.log(n1 - n2) // subtração
 console.log(n1 * n2) // multiplicação
 console.log(n1 / n2) // divisão
 console.log(n1 ** n2) // potencia
+console.log(15 % 2) // resto da divisão
---
+++ Curso_EspecializaTi/03/5-atribuição.js
@@ -0,0 +1,9 @@
+var n1 = 12
+var n2 = n1 
+
+// n1 = n1 - 1
+n1 += 1
+n1 -= 1
+n1 *= 2
+n1 /= 2
+console.log(n1)
