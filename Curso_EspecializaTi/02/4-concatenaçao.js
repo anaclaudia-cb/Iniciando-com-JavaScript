a1d88654ce02148087aa4802ba8d1dc7520e63a5
Ana Claudia <anaclaudia@yahp.com.br>
2021-12-16 14:00:47 -0300

    ...

---
+++ Curso_EspecializaTi/02/4-concatenaçao.js
@@ -0,0 +1,13 @@
+var name = 'Carlos'
+var age = 88
+var ageTwo = 90
+console.log('O ' + name +' tem ' + age + ' anos de idade.')
+
+// com apenas variaveis exemplo
+
+console.log(age + ' ' + ageTwo)
+
+// funçao para pegar informaçoes do usuario
+
+var nome = prompt('Qual seu nome')
+console.log(nome)
