43fb6d003a4d10f6d50547cead7a0774513793be
Ana Claudia <anaclaudia@yahp.com.br>
2021-12-16 15:26:30 -0300

    operadores js

---
+++ Curso_EspecializaTi/03/1-operadores.js
@@ -0,0 +1,7 @@
+var n1 = 12
+var n2 = 4
+console.log(n1 + n2) // soma
+console.log(n1 - n2) // subtração
+console.log(n1 * n2) // multiplicação
+console.log(n1 / n2) // divisão
+console.log(n1 ** n2) // potencia
---
+++ Curso_EspecializaTi/03/2-operadores.js
@@ -0,0 +1,20 @@
+var n1 = 12
+var n2 = 4
+
+console.log(2 + 4 - 3)
+console.log((4 - 3 + 2)* 2) // () define a prioridade da operação
+
+
+// incremento
+
+console.log(n1++)
+console.log(n1)
+
+// pre-incremento
+
+console.log(++n1)
+
+// decremento
+
+console.log(n1--)
+console.log(n1)
