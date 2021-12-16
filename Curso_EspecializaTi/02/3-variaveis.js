c08b6a632cf3850123d5a0b19db7ed86794c9dab
Ana Claudia <anaclaudia@yahp.com.br>
2021-12-16 13:41:59 -0300

    variaveis js

---
+++ Curso_EspecializaTi/02/3-variaveis.js
@@ -0,0 +1,30 @@
+var nome = 'Nome Pessoa';
+// var - variável
+console.log(nome);
+
+/* tipos de váriaveis:
+
+String              'Sou uma string'
+                    "Sou uma string"
+
+Inteiro             12
+Booleanos           true/false
+Pontos flutuantes   3.14
+
+Array               new Array()
+                    [1,2,3]
+
+Objetos             var car = {nome: "Fusion", ano: 2018, marca: 'Ford'}
+undefined           var teste
+
+
+Erros:
+
+Não iniciar com números     123nomeVar = 123
+Não utilizar espaços        nome Var = 321
+Não usar caracteres especiais   nome$@#Var = 232112
+Utilizar variaveis que criou    area = altura * largura
+Ter menos de 15 caracteres      nomeDeVariavelComMuitosCaracteres = 321
+Nome fazer sentido              a = 'Nome Pessoa'
+
+*/
