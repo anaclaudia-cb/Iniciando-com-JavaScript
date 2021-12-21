b3b5318e5cb3667a3c9168ecc7910d08c7d3dc60
Ana Claudia <anaclaudia@yahp.com.br>
2021-12-21 08:44:03 -0300

    ...

---
+++ Curso_EspecializaTi/03/6-comparação.js
@@ -0,0 +1,8 @@
+console.log( 2 == 2) // sinal de comparação
+console.log( 2 === '2') // compara o tipo
+console.log(2 != 2) // comparaçao de diferente
+console.log(2 !== '3') // comparaçao de tipo 
+console.log (2 > 4) // sinal de maior
+console.log(2 < 4) // sinal de menor
+console.log (3 >= 3) // maior e igual
+console.log(4 <= 4) // menor e igual
